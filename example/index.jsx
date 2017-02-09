94dc2ace86c221e59a18fc98cb33930447434e7d
Seven <seven@megalive.com.tw>
2017-02-09 18:10:08 +0800

    Added eslint feature

--- example/index.jsx
+++ example/index.jsx
@@ -1,42 +1,42 @@
 import { render } from 'react-dom';
 import React from 'react';
-import Pagination from '../src/index.js';
+import Pagination from '../lib';
 
 const App = class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.handleChangePage = ::this.handleChangePage;
-        this.state = {
-            activePage: 0
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.handleChangePage = ::this.handleChangePage;
+    this.state = {
+      activePage: 0,
+    };
+  }
 
-    handleChangePage(page) {
-        this.setState({
-            activePage: page
-        })
-    }
+  handleChangePage(page) {
+    this.setState({
+      activePage: page,
+    });
+  }
 
-    render() {
-        const { activePage } = this.state;
+  render() {
+    const { activePage } = this.state;
 
-        return (
-            <div>
-                <div>now page number: { activePage +1 }</div>
-                <Pagination
-                    handleChangePage = { this.handleChangePage }
-                    activePage = { activePage }
-                    totalCount = { 10 }
-                    perPageItemCount = { 2 }
-                    nextPageText = "next"
-                    prePageText = "prev"
-                />
-            </div>
-        )
-    }
-}
+    return (
+      <div>
+        <div>now page number: { activePage + 1 }</div>
+        <Pagination
+          handleChangePage={ this.handleChangePage }
+          activePage={ activePage }
+          totalCount={ 10 }
+          perPageItemCount={ 2 }
+          nextPageText="next"
+          prePageText="prev"
+        />
+      </div>
+    );
+  }
+};
 render(
-    <App />,
-    document.getElementById('container')
+  <App />,
+  document.getElementById('container'),
 );
