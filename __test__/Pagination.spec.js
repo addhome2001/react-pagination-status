33c42e50afdc23bcb94e1ff600e509a5f75d770d
Seven <addhome2001@yahoo.com.tw>
2018-04-05 17:28:31 +0800

    version 2.0

--- __test__/Pagination.spec.js
+++ __test__/Pagination.spec.js
@@ -22,17 +22,27 @@ const shallowComponent = props =>
   );
 
 const mountComponent = props =>
-  mount(<Pagination { ...props } perPageItemCount={ 10 } totalCount={ 30 } activePage={ 0 } />);
+  mount(
+    <Pagination
+      activePage={ 0 }
+      perPageItemCount={ 10 }
+      totalCount={ 40 }
+      handleChangePage={ handleChange }
+      { ...props }
+    />,
+  );
 
 describe('Test DataTable shareComponent', () => {
   beforeEach(() => handleChange.reset());
 
   it('correct props', () => {
     const wrapper = shallowComponent();
+
     expect(wrapper.instance().props).eql({
       activePage: 0,
       totalCount: 40,
       perPageItemCount: 10,
+      partialPageCount: 5,
       nextPageText: '下一頁',
       prePageText: '上一頁',
       className: 'react-pagination-status',
@@ -41,53 +51,210 @@ describe('Test DataTable shareComponent', () => {
   });
 
   it('with className: .active by defaultProps: activePage', () => {
-    const wrapper = shallowComponent({ activePage: 2 });
-    expect(wrapper.find('.react-pagination-status').childAt(3).hasClass('active')).to.equal(true);
+    const wrapper = mountComponent({ activePage: 2 });
+
+    expect(
+      wrapper
+        .find('PageButton')
+        .at(3)
+        .find('.react-pagination-status__btn--active')
+        .exists(),
+      ).to.equal(true);
   });
 
-  it('button amounts', () => {
-    const defaultProps = {
-      prePageText: 'prev',
-      nextPageText: 'next',
-    };
-      /* perPageItemCount / totalCount */
-    const pageCount = 40 / 10;
-    const wrapper = shallowComponent(defaultProps);
+  describe('correct info of the buttons', () => {
+    it('partial pages was no support', () => {
+      const defaultProps = {
+        prePageText: 'prev',
+        nextPageText: 'next',
+      };
+      const wrapper = mountComponent(defaultProps);
 
-    expect(wrapper.state('pageCount')).to.equal(pageCount);
-    expect(wrapper.find('.react-pagination-status').children('li')).to.have.length(pageCount + 2);
-    expect(wrapper.find('.react-pagination-status').children('li').first().text()).to.equal(defaultProps.prePageText);
-    expect(wrapper.find('.react-pagination-status').children('li').last().text()).to.equal(defaultProps.nextPageText);
+      expect(wrapper.find('PageButton')).to.have.length(6);
+      expect(wrapper.find('PageButton').map(n => n.text())).to.eql([
+        defaultProps.prePageText,
+        '1',
+        '2',
+        '3',
+        '4',
+        defaultProps.nextPageText,
+      ]);
+    });
+
+    it('in the first part', () => {
+      const defaultProps = {
+        prePageText: 'prev',
+        nextPageText: 'next',
+        activePage: 1,
+        perPageItemCount: 1,
+        totalCount: 20,
+      };
+      const wrapper = mountComponent(defaultProps);
+
+      expect(wrapper.find('PageButton')).to.have.length(9);
+      expect(wrapper.find('PageButton').map(n => n.text())).to.eql([
+        defaultProps.prePageText,
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '...',
+        '20',
+        defaultProps.nextPageText,
+      ]);
+    });
+
+    it('in the middle part', () => {
+      const defaultProps = {
+        prePageText: 'prev',
+        nextPageText: 'next',
+        activePage: 10,
+        perPageItemCount: 1,
+        totalCount: 20,
+      };
+      const wrapper = mountComponent(defaultProps);
+
+      expect(wrapper.find('PageButton')).to.have.length(11);
+      expect(wrapper.find('PageButton').map(n => n.text())).to.eql([
+        defaultProps.prePageText,
+        '1',
+        '...',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '...',
+        '20',
+        defaultProps.nextPageText,
+      ]);
+    });
+
+    it('in the tail part', () => {
+      const defaultProps = {
+        prePageText: 'prev',
+        nextPageText: 'next',
+        activePage: 20,
+        perPageItemCount: 1,
+        totalCount: 20,
+      };
+      const wrapper = mountComponent(defaultProps);
+
+      expect(wrapper.find('PageButton')).to.have.length(9);
+      expect(wrapper.find('PageButton').map(n => n.text())).to.eql([
+        defaultProps.prePageText,
+        '1',
+        '...',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20',
+        defaultProps.nextPageText,
+      ]);
+    });
   });
 
-  it('invoke handleChangePage when click on the nextPageButton or prePageButton', () => {
-    const wrapper = mountComponent({ handleChangePage: handleChange });
+  describe('invoke handleChangePage', () => {
+    it('clicks on the nextPageButton or prePageButton', () => {
+      const wrapper = mountComponent();
 
-    /* Click Prev Button */
-    wrapper.find('.react-pagination-status').children('li').first().simulate('click');
-    expect(handleChange.callCount).to.equal(1);
-    expect(handleChange.calledWith(2)).to.equal(true);
+      /* Click Prev Button */
+      wrapper.find('PageButton').last().simulate('click');
+      expect(handleChange.callCount).to.equal(1);
+      expect(handleChange.calledWith(1)).to.equal(true);
 
-    /* Click Next Button */
-    wrapper.find('.react-pagination-status').children('li').last().simulate('click');
-    expect(handleChange.callCount).to.equal(2);
-    expect(handleChange.calledWith(1)).to.equal(true);
-  });
+      /* increasing 1 to the activePage */
+      wrapper.setProps({ activePage: wrapper.instance().props.activePage + 1 });
 
+      /* Click Next Button */
+      wrapper.find('PageButton').first().simulate('click');
+      expect(handleChange.callCount).to.equal(2);
+      expect(handleChange.calledWith(0)).to.equal(true);
+    });
 
-  it('invoke handleChangePage when click on the page button', () => {
-    const wrapper = mountComponent({ handleChangePage: handleChange });
+    it('clicks on the first page button', () => {
+      const wrapper = mountComponent({
+        activePage: 10,
+        perPageItemCount: 1,
+        totalCount: 20,
+      });
 
-    wrapper.find('.react-pagination-status').hostNodes().childAt(2).simulate('click');
-    expect(handleChange.callCount).to.equal(1);
-    expect(handleChange.calledWith(1)).to.equal(true);
-    wrapper.setProps({ activePage: 1 });
-    expect(wrapper.find('.react-pagination-status').hostNodes().childAt(2).hasClass('active')).to.equal(true);
+      wrapper.find('PageButton').at(1).simulate('click');
 
-    wrapper.find('.react-pagination-status').hostNodes().childAt(3).simulate('click');
-    expect(handleChange.callCount).to.equal(2);
-    expect(handleChange.calledWith(2)).to.equal(true);
-    wrapper.setProps({ activePage: 2 });
-    expect(wrapper.find('.react-pagination-status').hostNodes().childAt(3).hasClass('active')).to.equal(true);
+      expect(handleChange.callCount).to.equal(1);
+      expect(handleChange.calledWith(0)).to.equal(true);
+    });
+
+    it('clicks on the last page button', () => {
+      const wrapper = mountComponent({
+        activePage: 10,
+        perPageItemCount: 1,
+        totalCount: 20,
+      });
+
+      wrapper.find('PageButton').at(9).simulate('click');
+      expect(handleChange.callCount).to.equal(1);
+      expect(handleChange.calledWith(19)).to.equal(true);
+    });
+
+    it('clicks on the ... button', () => {
+      const wrapper = mountComponent({
+        activePage: 10,
+        perPageItemCount: 1,
+        totalCount: 20,
+      });
+
+      wrapper.find('PageButton').at(2).simulate('click');
+      wrapper.find('PageButton').at(8).simulate('click');
+      expect(handleChange.callCount).to.equal(0);
+    });
+
+    it('clicks on the prePageButton button when on the first page', () => {
+      const wrapper = mountComponent();
+
+      wrapper.find('PageButton').at(0).simulate('click');
+      expect(handleChange.callCount).to.equal(0);
+    });
+
+    it('clicks on the nextPageButton button when on the last page', () => {
+      const wrapper = mountComponent({
+        activePage: 3,
+      });
+
+      wrapper.find('PageButton').at(5).simulate('click');
+      expect(handleChange.callCount).to.equal(0);
+    });
+
+    it('clicks on the page button', () => {
+      const wrapper = mountComponent();
+
+      wrapper.find('PageButton').at(2).simulate('click');
+      expect(handleChange.callCount).to.equal(1);
+      expect(handleChange.calledWith(1)).to.equal(true);
+
+      wrapper.setProps({ activePage: 1 });
+
+      expect(
+        wrapper
+          .find('PageButton')
+          .at(2)
+          .find('.react-pagination-status__btn--active')
+          .exists(),
+      ).to.equal(true);
+
+      wrapper.find('PageButton').at(3).simulate('click');
+      expect(handleChange.callCount).to.equal(2);
+      expect(handleChange.calledWith(2)).to.equal(true);
+      wrapper.setProps({ activePage: 2 });
+      expect(
+        wrapper
+        .find('PageButton')
+        .at(3)
+        .find('.react-pagination-status__btn--active')
+        .exists(),
+      ).to.equal(true);
+    });
   });
 });
